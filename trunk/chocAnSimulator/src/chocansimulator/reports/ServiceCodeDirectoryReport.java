ee624a370fc3bf2dff4374394a9b6cee0b415de6
Travis Johnson <travj@pdx.edu>
2011-02-24 15:32:08 -0800

    new file for service code directory

---
+++ trunk/chocAnSimulator/src/chocansimulator/reports/ServiceCodeDirectoryReport.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chocansimulator.reports;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author tman
+ */
+public class ServiceCodeDirectoryReport implements Reports {
+
+    public void formatHeader() {
+        //TODO
+    }
+
+    public void formatDetail() {
+        //TODO
+    }
+
+    public List getReportRecords() {
+        //TODO
+        List listData = new ArrayList();
+        return listData;
+    }
+    public void sortReportRecords() {
+        //TODO
+    }
+
+    public boolean createReport() {
+        //TODO
+        return true;
+    }
+}
